ef1e5a15530f7412a5bdf56b6033a7ccfc370e8a
YuanOwO <yuan@yuanowo.xyz>
2025-12-07 18:59:25 +0800

    新增配料的視圖與模板
    引入配料（ingredient）藍圖、配料清單與詳細頁面的視圖，以及對應的模板。更新供應商模板以連結配料，並改進供應商清單的標題。此更新增強了配料管理，並改善供應商與配料之間的導覽體驗。

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 from .errors import error_bp
 from .image import image_bp
+from .ingredient import ingredient_bp
 from .location import location_bp
 from .menu import menu_bp
 from .supplier import supplier_bp
@@ -10,6 +11,7 @@ from .supplier import supplier_bp
 def register(app: Flask):
     app.register_blueprint(error_bp)
     app.register_blueprint(image_bp)
+    app.register_blueprint(ingredient_bp)
     app.register_blueprint(location_bp)
     app.register_blueprint(menu_bp)
     app.register_blueprint(supplier_bp)
---
+++ app/views/ingredient.py
@@ -0,0 +1,40 @@
+from flask import Blueprint, abort, render_template, request, url_for
+
+from app.services import get_dishes_by_ingredient, get_ingredient_by_id, get_ingredients, get_suppliers_by_ingredient
+
+ingredient_bp = Blueprint("ingredient", __name__, url_prefix="/ingredients")
+
+
+@ingredient_bp.route("/")
+def ingredients():
+    """
+    食材列表頁面
+    """
+    keyword = request.args.get("query", "")
+    ingredients = get_ingredients(keyword=keyword)
+
+    return render_template("ingredient/list.html", ingredients=ingredients, keyword=keyword)
+
+
+@ingredient_bp.route("/<int:ingredient_id>")
+def ingredient_detail(ingredient_id: int):
+    """
+    食材詳細頁面
+    """
+    ingredient = get_ingredient_by_id(ingredient_id)
+
+    if ingredient is None:
+        abort(404)
+
+    suppliers = get_suppliers_by_ingredient(ingredient)
+    dishes = get_dishes_by_ingredient(ingredient)
+
+    deletable = len(dishes) == 0 and len(suppliers) == 0
+
+    return render_template(
+        "ingredient/detail.html",
+        ingredient=ingredient,
+        suppliers=suppliers,
+        dishes=dishes,
+        deletable=deletable,
+    )
